0373403042fd1849c0a7d597ff6b7b4fafd84ca4
akashi7 <christiannseko@gmail.com>
2024-10-02 12:46:53 +0200

    filters

--- src/components/dashboard/charts/energyChart.tsx
+++ src/components/dashboard/charts/energyChart.tsx
@@ -1,24 +1,43 @@
-import { FC, ReactElement, useEffect } from 'react'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Col, Form, Row } from 'antd'
+import { FC, ReactElement, useEffect, useState } from 'react'
 import {
   useGetEnergyFor30DaysQuery,
   useGetEnergyQuery,
 } from '../../../lib/api/Analytics/analyticsEndpoints'
+import CustomInput from '../../common/input/customInput'
 import { GeneralContentLoader } from '../../common/loader/loader'
 import Last30DaysGraph from './30days/PastThirtyDays'
 import Last7DaysGraph from './7days/PastSevenDays'
 
 const EnergyChart: FC = (): ReactElement => {
-  const { data, isFetching, refetch } = useGetEnergyQuery()
+  const [from, setFrom] = useState()
+  const [to, setTo] = useState()
+
+  const { data, isFetching, refetch } = useGetEnergyQuery(
+    from && to ? { from, to } : {}
+  )
   const {
     data: monthlyData,
     isFetching: fetching,
     refetch: refechMonthly,
-  } = useGetEnergyFor30DaysQuery()
+  } = useGetEnergyFor30DaysQuery(from && to ? { from, to } : {})
 
   useEffect(() => {
     refetch()
     refechMonthly()
   }, [refetch, refechMonthly])
+
+  function onChangeFromDate(e: any) {
+    setFrom(e)
+  }
+  function onChangeToDate(e: any) {
+    setTo(e)
+  }
+
+  console.log({ from }, { to })
+
+  const [form] = Form.useForm()
 
   if (isFetching || fetching) {
     return <GeneralContentLoader />
@@ -27,9 +46,31 @@ const EnergyChart: FC = (): ReactElement => {
   return (
     <section className='w-[100%]'>
       <div className=''>
-        <h1 className=' text-xl mb-10 text-[#C1CF16] font-bold'>
-          Energy graphs
-        </h1>
+        <section className='flex flex-col gap-5 mb-10'>
+          <h1 className=' text-xl text-[#C1CF16] font-bold'>Energy graphs</h1>
+          <Form className='space-y-12' name='user-info-form' form={form}>
+            <Row className='w-[100%]  flex flex-row gap-5'>
+              <Col className='gutter-row mt-2 w-[25%]'>
+                <CustomInput
+                  placeholder='From'
+                  label='From'
+                  inputType='date'
+                  name='from'
+                  onChange={onChangeFromDate}
+                />
+              </Col>
+              <Col className='gutter-row mt-2 w-[25%]'>
+                <CustomInput
+                  placeholder='To'
+                  label='To'
+                  inputType='date'
+                  name='to'
+                  onChange={onChangeToDate}
+                />
+              </Col>
+            </Row>
+          </Form>
+        </section>
         <section className='flex flex-col'>
           <div>
             <h2 className='text-lg font-bold mb-5'>Last 7 Days</h2>
--- src/components/notfound/ErrorPage.tsx
+++ src/components/notfound/ErrorPage.tsx
@@ -1,13 +1,25 @@
 import { FC, ReactElement } from 'react'
+import { useNavigate } from 'react-router-dom'
+import CustomButton from '../common/button/button'
 
 const ErrorPage: FC = (): ReactElement => {
+  const navigate = useNavigate()
   return (
-    <pre className='text-[160px] font-bold text_404 text-center'>
-      404
-      <br />
-      <br />
-      Page Not found
-    </pre>
+    <div className='w-[100%] h-[600px] grid items-center justify-center'>
+      <div>
+        <h1 className='text-[160px] font-bold text_404 text-center'>404</h1>
+        <p className='text-[16px] text-center text-gray-500'>Page not found</p>
+        <div className='mt-6 w-[100%] grid items-center justify-center'>
+          <CustomButton
+            type='primary'
+            className='h-[60px]'
+            onClick={() => navigate('/')}
+          >
+            Go home
+          </CustomButton>
+        </div>
+      </div>
+    </div>
   )
 }
 
